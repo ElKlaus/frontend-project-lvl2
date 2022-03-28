4a674ea756825eb999fe80ecba0acdde9b27f0c5
ElKlaus <klaus15rus@gmail.com>
2022-03-28 18:48:04 +0300

    Step 2: add arguments and custom discribe for help

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,10 @@ import { program } from "commander";
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.1');
+  .version('1.0.1')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
