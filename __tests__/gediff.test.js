1fc731c1ba2c14a1cc866e55363338304dd5df1e
ElKlaus <klaus15rus@gmail.com>
2022-04-01 20:35:30 +0300

    Step 3: config Jest

--- __tests__/gediff.test.js
+++ __tests__/gediff.test.js
@@ -1,6 +1,16 @@
-import { test, expect } from '@jest/globals';
+import { test, expect, beforeEach } from '@jest/globals';
 import { genDiff } from '../src/gendiff.js'
 
-test('some test', ()=>{
-  expect('somestring').toBe('somestring');
+let data = 
+`{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+
+test('genDiff', ()=>{
+  expect(genDiff('file1.json', 'file2.json')).toBe(data);
 });
