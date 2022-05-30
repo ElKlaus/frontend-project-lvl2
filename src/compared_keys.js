4b1131e1737a588d4d22476532ee68c3c7629618
ElKlaus <klaus15rus@gmail.com>
2022-05-30 20:38:53 +0300

    Step 6: successful tests

--- src/compared_keys.js
+++ src/compared_keys.js
@@ -5,7 +5,7 @@ const objToArr = (data) => {
   const result = [];
 
   for (const [key, value] of entries) {
-    if(typeof value === 'object') {
+    if (typeof value === 'object') {
       result.push([' ', key, objToArr(value)]);
     } else {
       result.push([' ', key, value]);
@@ -25,11 +25,11 @@ const comparedKeys = (firstObj, secondObj) => {
 
   const diffArr = sortKeys.reduce((acc, el) => {
     if (typeof firstObj[el] === 'object' && typeof secondObj[el] === 'object') {
-      acc.push([el, (comparedKeys(firstObj[el], secondObj[el]))]);
+      acc.push([' ', el, (comparedKeys(firstObj[el], secondObj[el]))]);
     } else if ((_.has(firstObj, el) && _.has(secondObj, el)) && (firstObj[el] === secondObj[el])) {
       acc.push([' ', el, firstObj[el]]);
     } else if ((_.has(firstObj, el) && _.has(secondObj, el)) && (firstObj[el] !== secondObj[el])) {
-      console.log(firstObj[el], secondObj[el]);
+      // console.log(firstObj[el], secondObj[el]);
 
       const current1 = typeof firstObj[el] === 'object' && firstObj[el] !== null ? objToArr(firstObj[el]) : firstObj[el];
       const current2 = typeof secondObj[el] === 'object' && secondObj[el] !== null ? objToArr(secondObj[el]) : secondObj[el];
--- src/stylish.js
+++ src/stylish.js
@@ -1,36 +1,25 @@
-import _ from 'lodash';
-
-const stylish = (data, replacer = ' ', spacesCount = 1) => {
-  let result = '';
-
+const stylish = (data, replacer = ' ', spacesCount = 2) => {
   const iter = (coll, count) => {
     const spaces = replacer.repeat(count);
     const newSpacesCount = count + spacesCount;
 
-    // const current = (typeof coll === 'object') ? Object.entries(coll) : coll;
-
-    // console.log(current);
+    // console.log(coll);
 
     return coll.reduce((acc, el) => {
       const [sign, key, value] = el;
       let newAcc = '';
 
-      if (Array.isArray(key)) {
-        newAcc = `${acc}${spaces}  ${sign}**: {\n${iter(key, newSpacesCount)}${spaces}}\n`;
-      } else if (Array.isArray(value)) {
-        newAcc = `${acc}${spaces}${key}**: {\n${iter(value, newSpacesCount)}${spaces}}\n`;
+      if (Array.isArray(value)) {
+        newAcc = `${acc}${spaces}${sign} ${key}: {\n${iter(value, newSpacesCount + 2)}  ${spaces}}\n`;
       } else {
-        // console.log(key,'////', value);
-        newAcc = `${acc}${spaces}${sign}${key}*: ${value}\n`;
+        newAcc = `${acc}${spaces}${sign} ${key}: ${value}\n`;
       }
 
       return newAcc;
     }, '');
   };
 
-  result = `{\n${iter(data, spacesCount)}}`;
-
-  return result;
+  return `{\n${iter(data, spacesCount)}}`;
 };
 
 export default stylish;
